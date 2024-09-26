ce68109e40c93bf64ec3a0618f47dc7eafa46275
Raulitoflying <1017329098@qq.com>
2024-09-26 16:17:46 -0700

    Refactor App.js to use React Navigation for screen navigation

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import StartScreen from './Screens/Start/StartScreen'; 
+import ConfirmScreen from './Screens/Confirm/ConfirmScreen';
+import GameScreen from './Screens/Game/GameScreen';
+import GameOverScreen from './Screens/Game/GameOverScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Start" component={StartScreen} />
+        <Stack.Screen name="Confirm" component={ConfirmScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+        <Stack.Screen name="GameOver" component={GameOverScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
